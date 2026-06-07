b6b688bb418d53532723a581e46ab2b8a507818b
Ahmed Bilal <ahmedbilal3830@gmail.com>
2026-06-07 18:45:21 +0500

    test: add integration tests between API gateway, authentication and patient microservice.

---
+++ integration-tests/src/test/java/com/pm/integration_tests/PatientIntegrationTest.java
@@ -0,0 +1,36 @@
+package com.pm.integration_tests;
+
+import static org.hamcrest.Matchers.notNullValue;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import io.restassured.RestAssured;
+
+public class PatientIntegrationTest {
+
+	@BeforeAll
+	static void setUp() {
+
+		RestAssured.baseURI = "http://localhost:4000";
+
+	}
+
+	@Test
+	public void shouldReturnPatientsWithValidToken() {
+
+		String loginPayload = """
+				{
+					"email": "ahmedbilal@gmail.com",
+					"password": "password123"
+				}
+				""";
+		String token = RestAssured.given().contentType("application/json").body(loginPayload).when().post("/auth/login")
+				.then().statusCode(200).extract().jsonPath().get("token");
+
+		RestAssured.given().header("Authorization", "Bearer " + token).when().get("/api/patients").then()
+				.statusCode(200).body("patients", notNullValue());
+
+	}
+
+}
